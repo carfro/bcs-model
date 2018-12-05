320f84f6c2dccb3a6f53fb1fec92098b38a69ef7
carfro <carl.frostenson@me.com>
2018-12-05 18:32:01 +0100

    The W^T*W matrix is now skew-symmetric, pf(W^T*W) is still zero though

--- BCSmodel.f90
+++ BCSmodel.f90
@@ -14,9 +14,9 @@ program main
 
 ! 	Variables used for analytic solution
 	REAL(8), dimension(:,:), allocatable :: EV_N,EV_Z
-	COMPLEX(8) ::  U(2*N_tot,2*N_tot) , V(2*N_tot,2*N_tot) , SK(4*N_tot,4*N_tot), Pf, &
-		A(2*N_tot,2*N_tot),B(2*N_tot,2*N_tot),C(2*N_tot,2*N_tot), SK2(4*N_tot,4*N_tot),&
-		SK3(4,4),Pf3,Ipiv(4*N_tot,2),Ipiv3(4,2),SKD(4*N_tot,4*N_tot),W(4*N_tot,4*N_tot)
+	COMPLEX(8) ::  U(N_tot,N_tot) , V(N_tot,N_tot) , SK(2*N_tot,2*N_tot), Pf, &
+		A(N_tot,N_tot),B(N_tot,N_tot),C(N_tot,N_tot), SK2(2*N_tot,2*N_tot),&
+		SK3(4,4),Pf3,Ipiv(2*N_tot,2),Ipiv3(4,2),SKD(2*N_tot,2*N_tot),W(2*N_tot,2*N_tot)
 
 	REAL(8) :: V_N_tot(N_tot,7), Theta_N(N_tot,7) 
 	REAL(8) :: lam_sN,lam_sZ,tol
@@ -45,31 +45,33 @@ program main
 	end do
 
 	U=0;V=0;
-	do i=1,N_tot
+	do i=1,N_tot,2
 		Theta_N(i,:)=(/  (0.5*dacos(-1.d0+2*V_N_tot(i,j)), j=1,7) /)
+		Theta_N(i+1,:)=(/  (0.5*dacos(-1.d0+2*V_N_tot(i+1,j)), j=1,7) /)
 		U(i,i)=cmplx(dsin(theta_N(i,3)),0,8)
-		U(N_tot+i,N_tot+i)=cmplx(dsin(theta_N(i,3)),0,8)
-		V(i,i+1)=cmplx(-1*dcos(theta_N(i,3)),0,8)
-		V(i+1,i)=cmplx(dcos(theta_N(i,3)),0,8)
-		if(N_tot+i<2*N_tot) then
-			V(i+N_tot,i+1+N_tot)=cmplx(-1*dcos(theta_N(i,3)),0,8)
-			V(i+1+N_tot,i+N_tot)=cmplx(dcos(theta_N(i,3)),0,8)
+		U(i+1,i+1)=cmplx(dsin(theta_N(i,3)),0,8)
+		if(i+1<N_tot) then
+			V(i,i+1)=cmplx(dcos(theta_N(i,3)),0,8)
+			V(i+1,i)=cmplx(-1*dcos(theta_N(i,3)),0,8)
 		end if
+		!if(i+1<N_tot) then
+		!	V(i+N_tot,i+1+N_tot)=cmplx(-1*dcos(theta_N(i,3)),0,8)
+		!	V(i+1+N_tot,i+N_tot)=cmplx(dcos(theta_N(i,3)),0,8)
+		!end if
 	!	if(real(U(i,i),8)<0 .or.  real(V(i,i),8)<0) then 
 	!		write(*,*) i, j 
 	!		write(*,*) real(U(i,i),8) , real(V(i,i),8)
 	!	end if
 	end do
 
-	open(unit=1,file='data/SK.dat')
 	A=matmul(transpose(V),U)
 	B=matmul(transpose(U),V)
 	C=matmul(transpose(V),V)
 
-	SK(1:2*N_tot,1:2*N_tot) = A
-	SK(2*N_tot+1:4*N_tot,1:2*N_tot) = -C
-	SK(1:2*N_tot,2*N_tot+1:4*N_tot) = C
-	SK(2*N_tot+1:4*N_tot,2*N_tot+1:4*N_tot) = B
+	SK(1:N_tot,1:N_tot) = A
+	SK(N_tot+1:2*N_tot,1:N_tot) = -C
+	SK(1:N_tot,N_tot+1:2*N_tot) = C
+	SK(N_tot+1:2*N_tot,N_tot+1:2*N_tot) = B
 
 	W=reshape((/  U ,V ,&
 			V , U /), shape(W))
@@ -79,49 +81,52 @@ program main
 			(0,0),(2,0),(0,0),(-3,0),&
 			(0,0),(0,0),(3,0),(0,0)/), shape(SK3))	
 
-	!call PRINT_MATRIX('Skew-sym matrix' , 4,4,SK3,4)
+	open(unit=1,file='data/SK.dat',status='replace')
+	open(unit=2,file='data/SKD.dat',status='replace')
+	open(unit=3,file='data/A.dat',status='replace')
+	open(unit=4,file='data/B.dat',status='replace')
+	open(unit=5,file='data/U.dat',status='replace')
+	open(unit=6,file='data/V.dat',status='replace')
 
-	open(unit=2,file='data/SKD.dat')
-	open(unit=3,file='data/A.dat')
-	open(unit=4,file='data/B.dat')
 	SKD=SK+transpose(SK)
 
-	do i=1,4*N_tot
-		do j=1,4*N_tot
+	do i=1,2*N_tot
+		do j=1,2*N_tot
 			if(.not.(abs(SK(i,j))<1.d-5)) then 
-				write(1,"(I5,I5)") j,4*N_tot-1*i 
+				write(1,"(I5,I5)") j,2*N_tot-1*i 
 			end if
-			if(.not.(abs(W(i,j))<1.d-5)) then 
-				write(2,"(I5,I5)") j,4*N_tot-1*i
+			if(.not.(abs(SKD(i,j))<1.d-5)) then 
+				write(2,"(I5,I5)") j,2*N_tot-1*i
 			end if
 
-			if (i<=2*N_tot .and. j<=2*N_tot) then
+			if (i<=N_tot .and. j<=N_tot) then
+				if(.not.(abs(A(i,j))<1.d-5)) then 
+					write(3,"(I5,I5)") j,N_tot-1*i
+					!write(3,"(I5,I5,F17.17)") j,N_tot-1*i, real(A(i,j),8) 
+				end if
+				if(.not.(abs(B(i,j))<1.d-5)) then 
+					write(4,"(I5,I5)") j,N_tot-1*i 
+				end if
 				if(.not.(abs(U(i,j))<1.d-5)) then 
-					write(3,"(I5,I5)") j,2*N_tot-1*i
+					write(5,"(I5,I5)") j,N_tot-1*i
 					!write(3,"(I5,I5,F17.17)") j,N_tot-1*i, real(A(i,j),8) 
 				end if
 				if(.not.(abs(V(i,j))<1.d-5)) then 
-					write(4,"(I5,I5)") j,2*N_tot-1*i 
+					write(6,"(I5,I5)") j,N_tot-1*i,real(V(i,j),8) 
 				end if 
 			end if 
 		end do
 	end do
-!	do i=1,2*N_tot
-!		do j=1,2*N_tot
-!			if(.not.(abs(U(i,j))<1.d-5)) then 
-!				write(3,"(I5,I5)") j,2*N_tot-1*i
-!				!write(3,"(I5,I5,F17.17)") j,N_tot-1*i, real(A(i,j),8) 
-!			end if
-!			if(.not.(abs(V(i,j))<1.d-5)) then 
-!				write(4,"(I5,I5)") j,2*N_tot-1*i 
-!			end if 
-!		end do
-!	end do
-	call ZPfaffianF(SK,4*N_tot,4*N_tot,Ipiv,Pf)
+	call ZPfaffianF(SK,2*N_tot,2*N_tot,Ipiv,Pf)
 	call ZPfaffianF(SK3,4,4,Ipiv3,Pf3)
 
 	write(*,*) Pf
 	write(*,*) Pf3
 	close(unit=1)
+	close(unit=2)
+	close(unit=3)
+	close(unit=4)
+	close(unit=5)
+	close(unit=6)
 	deallocate(nucleus,EV_N)
 end program main
