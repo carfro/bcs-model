f8ecfa0a313073c6540362884910bffeee3ffa04
carfro <carl.frostenson@me.com>
2018-12-27 17:23:08 +0100

    Last commit was uncomplete. Added libs and scrapbook containing old cod that might be useful

--- BCSmodel.f90
+++ BCSmodel.f90
@@ -5,6 +5,7 @@ module BCS
 	USE F95_PFAPACK
 	implicit none
 	integer, parameter :: N_tot=330
+<<<<<<< ed64cb91f3bfcf01ea739c71ab7d2bf93469f696
 	real(8), parameter :: PI=4.D0*DATAN(1.D0)
 	real(kind=qp), parameter :: ten_quad = 10.
 
@@ -175,14 +176,73 @@ contains
 
 	END FUNCTION DMAT_CREATOR
 	
+=======
+contains
+
+	Subroutine qpart_creator(nucleus,N,Z,scalefactor,U_N,V_N,prod_N,U_Z,V_Z,prod_Z)
+
+		type(Nucleon), dimension(:,:),intent(in) 	:: nucleus
+		real(8),intent(in)				:: scaleFactor
+		integer,intent(in) 				:: N,Z
+
+		complex(kind=qp),intent(out)			:: prod_N,prod_Z
+		complex(8),intent(out) 				:: U_N(N_tot,N_tot),V_N(N_tot,N_tot),&
+								U_Z(N_tot,N_tot),V_Z(N_tot,N_tot)
+
+		real(8) 	:: Theta_N(N_tot),Theta_Z(N_tot),&
+				lam_sN,lam_sZ,tol,EV_N(N_tot,2),EV_Z(N_tot,2)
+		integer 	:: i,j,step
+
+		step=1000 	! Nbr of points in \lambda vector
+		tol=0.001	! tolerance to find root
+
+		call analytic_solve_sweep(nucleus,N,Z,step,tol,&
+			EV_N,lam_sN,EV_Z,lam_sZ,scaleFactor)
+
+		U_N=0;V_N=0;prod_N=1;
+		U_Z=0;V_Z=0;prod_Z=1;
+		do i=1,N_tot,2
+			Theta_N(i)= 0.5*dacos(-1.d0+2*EV_N(i,2))
+			Theta_N(i+1)= 0.5*dacos(-1.d0+2*EV_N(i+1,2))
+			Theta_Z(i)= 0.5*dacos(-1.d0+2*EV_Z(i,2))
+			Theta_Z(i+1)= 0.5*dacos(-1.d0+2*EV_Z(i+1,2))
+			
+			U_N(i,i)=cmplx(dsin(theta_N(i)),0,8)
+			U_N(i+1,i+1)=cmplx(dsin(theta_N(i)),0,8)
+			U_Z(i,i)=cmplx(dsin(theta_Z(i)),0,8)
+			U_Z(i+1,i+1)=cmplx(dsin(theta_Z(i)),0,8)
+			if(i+1<=N_tot) then
+				prod_N=prod_N*EV_N(i,2)
+				V_N(i,i+1)=cmplx(dcos(theta_N(i)),0,8)
+				V_N(i+1,i)=cmplx(-1*dcos(theta_N(i)),0,8)
+
+				prod_Z=prod_Z*EV_Z(i,2)
+				V_Z(i,i+1)=cmplx(dcos(theta_Z(i)),0,8)
+				V_Z(i+1,i)=cmplx(-1*dcos(theta_Z(i)),0,8)
+			end if
+		end do
+	END Subroutine qpart_creator
+
+	FUNCTION WTW(U,V) result(WW)
+		complex(8) 			:: WW(2*N_tot,2*N_tot),U(N_tot,N_tot),V(N_tot,N_tot)
+
+		WW(1:N_tot,1:N_tot) = matmul(transpose(V),U)
+		WW(N_tot+1:2*N_tot,1:N_tot) = -matmul(transpose(V),V)
+		WW(1:N_tot,N_tot+1:2*N_tot) = matmul(transpose(V),U)
+		WW(N_tot+1:2*N_tot,N_tot+1:2*N_tot) = matmul(transpose(U),V)
+	END FUNCTION WTW
+
+>>>>>>> Last commit was uncomplete. Added libs and scrapbook containing old cod that might be useful
 end module BCS
 ! Constructs the BCS-model and computes the system for \lambda=[1..-1] and plots the result versus number of particles
 program main
 	use BCS
+	use f95_pfapack
 	implicit none
 ! 	Nucleus, filled using MO_module
 	type(Nucleon), dimension(:,:), allocatable :: nucleus
 ! 	Variables used for analytic solution
+<<<<<<< ed64cb91f3bfcf01ea739c71ab7d2bf93469f696
 	COMPLEX(dp) :: 	Pf2P(2),Pf2P_ol,&
 			U_N(N_tot,N_tot),V_N(N_tot,N_tot),&
 			U_Z(N_tot,N_tot),V_Z(N_tot,N_tot),&
@@ -252,8 +312,54 @@ program main
 		!if(j .NE. N_exp+2) write(2,'(2/)')
 		!write(2,*)
 	end do
+=======
+	COMPLEX(8) :: 	Pf2,WW_N(2*N_tot,2*N_tot),&
+			U_N(N_tot,N_tot),V_N(N_tot,N_tot),&
+			U_Z(N_tot,N_tot),V_Z(N_tot,N_tot)
+
+	complex(kind=qp) :: Pf22,Pf,prod_N,prod_Z
+	real(kind=qp) 	:: factor
+
+	Integer 	:: Ipiv(2*N_tot,2),Ipiv2(2*24,2),Ipiv22(2*24,2),Ipiv3(4,2)
+
+	REAL(8) 	:: scaleFactor(7)
+
+	! Nbr of neutrons/protons and loop integer(s),
+	!integer :: i,j
+	integer :: N,Z,siz,step
+
+	N=24		! Number of NEUTRONS to find
+	Z=24 		! Number of PROTONS to find
+
+!-------Analytical solution using BCS-equations
+	allocate(nucleus(N_tot,2))
+
+	call nucleus_creator(N,Z,nucleus)
+
+	scaleFactor = (/0.5d0,0.75d0,1.d0,1.75d0,2.5d0,3d0,4d0/)
+	
+	call qpart_creator(nucleus,N,Z,scalefactor(3),U_N,V_N,prod_N,U_Z,V_Z,prod_Z)
+	WW_N = WTW(U_N,V_N)
+
+	open(unit=1,file='data/performance.dat',status='replace')
+
+	factor=1
+	Pf=overlap_pfaffian(factor,N_tot,N_tot,N_tot,U_N,U_N,V_N,V_N)
+
+	!call ZPfaffian_EXT(WW,2*N_tot,2*N_tot,Ipiv,Pf)
+
+	call SKPF10(WW_N,Pf2) 
+	
+	write(*,*) 'Product: ', real(prod_N)
+	write(*,*) 'Pf_Extended: ', real(Pf)
+	write(*,*) 'Pf_SKPF10: ', real(Pf2)
+>>>>>>> Last commit was uncomplete. Added libs and scrapbook containing old cod that might be useful
 
 	deallocate(nucleus)
 	close(unit=1)
+<<<<<<< ed64cb91f3bfcf01ea739c71ab7d2bf93469f696
 	close(unit=2)
+=======
+	deallocate(nucleus)
+>>>>>>> Last commit was uncomplete. Added libs and scrapbook containing old cod that might be useful
 end program main
--- makefile
+++ makefile
@@ -7,6 +7,7 @@
 #              Default:   Intel Fortran  compiler 
 #              		LINUX
 # ----------------------------------------------------------------------
+<<<<<<< ed64cb91f3bfcf01ea739c71ab7d2bf93469f696
 FC=ifort
 FFLAGS= -g -check all -fp-stack-check -heap-arrays 
 ##         Intel's math kernel library, for LINUX
@@ -15,10 +16,17 @@ FFLAGS= -g -check all -fp-stack-check -heap-arrays
 LIBS= -mkl -lmkl_core -lmkl_intel_thread -lmkl_intel_lp64 -liomp5 
 #PFAPATH=/nfs/users3/carlfrost/Documents/Code/Pfaffian/pfapack/fortran
 PFAPATH=lib/fortran
+=======
+#FC=ifort
+#FLAGS= -g -check all -fp-stack-check -heap-arrays $(IFLAGS)
+##         Intel's math kernel library, for LINUX
+#LIBS= -L/opt/intel/Compiler/11.0/069/mkl/lib/em64t/ 
+>>>>>>> Last commit was uncomplete. Added libs and scrapbook containing old cod that might be useful
 # ----------------------------------------------------------------------
 #              Default:   Intel Fortran  compiler 
 #              		MACINTOSH	
 # ----------------------------------------------------------------------
+<<<<<<< ed64cb91f3bfcf01ea739c71ab7d2bf93469f696
 #FC=ifort
 #FFLAGS= -g -check all -fp-stack-check -heap-arrays 
 ##         Intel's math kernel library, for MAC
@@ -26,11 +34,24 @@ PFAPATH=lib/fortran
 #PFAPATH=/Users/carlfrostenson/Documents/1_UNI/1_MasterThesis/Fortran/Routines/Pfaffian/pfapack/fortran
 ##
 #LIBS= -mkl -Wl,-rpath,$(MKLPATH) -lmkl_core -lmkl_intel_thread -lmkl_intel_lp64 -liomp5 
+=======
+FC=ifort
+FLAGS= -g -check all -fp-stack-check -heap-arrays $(IFLAGS)
+#         Intel's math kernel library, for MAC
+MKLPATH=/opt/intel/compilers_and_libraries_2019.1.144/mac/mkl/lib 
+PFAPATH=/Users/carlfrostenson/Documents/1_UNI/1_MasterThesis/Fortran/Routines/Pfaffian/pfapack/fortran
+#
+LIBS= -mkl -Wl,-rpath,$(MKLPATH) -lmkl_core -lmkl_intel_thread -lmkl_intel_lp64 -liomp5 
+>>>>>>> Last commit was uncomplete. Added libs and scrapbook containing old cod that might be useful
 # ----------------------------------------------------------------------
 #            Object files and Modules                                   
 # ----------------------------------------------------------------------
 MOD_DIR=modules
+<<<<<<< ed64cb91f3bfcf01ea739c71ab7d2bf93469f696
 PFAFLAGS=-I$(PFAPATH) -L$(PFAPATH)
+=======
+IFLAGS=-I$(MOD_DIR) -I$(PFAPATH) $(PFAPATH)/libpfapack.a
+>>>>>>> Last commit was uncomplete. Added libs and scrapbook containing old cod that might be useful
 MFLAG=-module $(MOD_DIR)
 #  
 OBJ_DIR=$(MOD_DIR)
@@ -42,12 +63,19 @@ MAIN=$(wildcard *.f90)
 #            Targets
 # ----------------------------------------------------------------------
 # "make" will build all
+<<<<<<< ed64cb91f3bfcf01ea739c71ab7d2bf93469f696
 all: $(MAIN)
 	
 $(MAIN):$(OBJECTS) 
 	$(FC) $(FFLAGS) $(PFAFLAGS) $(MFLAG) -o $(patsubst %.f90,%,$@) $? $@ $(LIBS) -lpfapack
 %.o: %.f90
 	$(FC) $(FFLAGS) $(LIBS) $(MFLAG) -c $<  -o $@
+=======
+all: $(OBJECTS) $(MAIN)
+	$(FC) $(FLAGS) $(LIBS) $(MFLAG) -o BCS $(OBJECTS) $(MAIN)
+%.o: %.f90	
+	$(FC) $(FLAGS) $(LIBS) $(MFLAG) -c $<  -o $@
+>>>>>>> Last commit was uncomplete. Added libs and scrapbook containing old cod that might be useful
 # Utility targets
 clean: 
 	rm -f $(OBJ_DIR)/*.o $(MOD_DIR)/*.mod *.o *.mod $(MAIN_TARGET)
